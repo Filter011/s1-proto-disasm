ec0e521f913811a5657ba0dd4085c37f84c6b9cc
Filter <not.so.filter@hotmail.com>
2026-09-09 11:10:53 -0400

    bring AnimateSprite up to date

--- obj/sub AnimateSprite.asm
+++ obj/sub AnimateSprite.asm
@@ -1,74 +1,103 @@
 ; ===========================================================================
+; ---------------------------------------------------------------------------
+; Subroutine to animate a sprite using an animation script
+; 
+; input:
+; 	a0 = pointer to object
+; 	a1 = pointer to animation script
+; ---------------------------------------------------------------------------
 
 AnimateSprite:
 		moveq	#0,d0
-		move.b	obAnim(a0),d0
-		cmp.b	obPrevAni(a0),d0
-		beq.s	loc_6B54
-		move.b	d0,obPrevAni(a0)
-		move.b	#0,obAniFrame(a0)
-		move.b	#0,obTimeFrame(a0)
+		move.b	obAnim(a0),d0				; move animation number to d0
+		cmp.b	obPrevAni(a0),d0			; has animation changed?
+		beq.s	Anim_LoadNextFrame			; if not, branch
+		move.b	d0,obPrevAni(a0)			; remember new animation ID
+		move.b	#0,obAniFrame(a0)			; reset animation frame index
+		move.b	#0,obTimeFrame(a0)			; reset animation frame duration
+; ---------------------------------------------------------------------------
 
-loc_6B54:
-		add.w	d0,d0
-		adda.w	(a1,d0.w),a1
-		subq.b	#1,obTimeFrame(a0)
-		bpl.s	locret_6B94
-		move.b	(a1),obTimeFrame(a0)
+Anim_LoadNextFrame:
+		add.w	d0,d0					; double animation ID for word-based indexing
+		adda.w	(a1,d0.w),a1				; jump to appropriate animation script
+		subq.b	#1,obTimeFrame(a0)			; subtract 1 from frame duration
+		bpl.s	Anim_Wait				; if time remains, do nothing
+		move.b	(a1),obTimeFrame(a0)			; load frame duration (always the first byte)
 		moveq	#0,d1
-		move.b	obAniFrame(a0),d1
-		move.b	1(a1,d1.w),d0
-		bmi.s	loc_6B96
+		move.b	obAniFrame(a0),d1			; load current frame index number
+		move.b	1(a1,d1.w),d0				; read next frame ID from script
+		bmi.s	Anim_End_FF				; if ID is negative, this is a special flag, branch
+; ---------------------------------------------------------------------------
 
-loc_6B70:
-		move.b	d0,d1
-		andi.b	#$1F,d0
-		move.b	d0,obFrame(a0)
-		move.b	obStatus(a0),d0
-		andi.b	#3,d0
-		andi.b	#$FC,obRender(a0)
-		lsr.b	#5,d1
-		eor.b	d0,d1
-		or.b	d1,obRender(a0)
-		addq.b	#1,obAniFrame(a0)
+Anim_SetFrameAndFlipFlags:
+		move.b	d0,d1					; copy new frame ID
+		andi.b	#$1F,d0					; limit possible frame IDs to $20 (other bits are occupied by flags)
+		move.b	d0,obFrame(a0)				; write new frame ID to object
 
-locret_6B94:
-		rts
+		; Handle aniXFlip and aniYFlip flags if specified in animation scripts.
+		; This part is the main reason why non-Sonic objects are limited to only $20 frames,
+		; and in later games this limitation was removed (flipping was instead exclusively
+		; handled through separate, flipped sprite mappings sets, rather than animation flags).
+		move.b	obStatus(a0),d0				; get object's current status flags
+		andi.b	#sprite_xflip|sprite_yflip,d0		; limit result to only X and Y flip state
+		andi.b	#~(sprite_xflip|sprite_yflip),obRender(a0) ; clear previous X and Y flip states of objects
+		lsr.b	#5,d1					; shift aniXFlip and aniYFlip into low bits to match obStatus format
+		eor.b	d0,d1					; xor with existing flip state of object
+		or.b	d1,obRender(a0)				; set new X and Y flip states for object
+
+		addq.b	#1,obAniFrame(a0)			; advance animation script to next index
+; ---------------------------------------------------------------------------
+
+Anim_Wait:
+		rts						; animation processing done
+
+; ===========================================================================
+; ---------------------------------------------------------------------------
+; Handle special animation flags ($FA-$FF)
+; ---------------------------------------------------------------------------
+
+; afEnd = return to beginning of animation (loop indefinitely)
+Anim_End_FF:
+		addq.b	#1,d0					; is the end flag = $FF?
+		bne.s	Anim_End_FE				; if not, branch
+		move.b	#0,obAniFrame(a0)			; restart the animation from the beginning
+		move.b	1(a1),d0				; read first frame ID in script
+		bra.s	Anim_SetFrameAndFlipFlags		; display new frame
 ; ===========================================================================
 
-loc_6B96:
-		addq.b	#1,d0
-		bne.s	loc_6BA6
-		move.b	#0,obAniFrame(a0)
-		move.b	obRender(a1),d0
-		bra.s	loc_6B70
+; afBack = go back (specified number) of frames in animation script
+Anim_End_FE:
+		addq.b	#1,d0					; is the end flag = $FE?
+		bne.s	Anim_End_FD				; if not, branch
+		move.b	2(a1,d1.w),d0				; read the next byte in the script
+		sub.b	d0,obAniFrame(a0)			; decrement script index by that amount
+		sub.b	d0,d1					; jump back d0 bytes in the script
+		move.b	1(a1,d1.w),d0				; read new frame ID in script
+		bra.s	Anim_SetFrameAndFlipFlags		; display new frame
 ; ===========================================================================
 
-loc_6BA6:
-		addq.b	#1,d0
-		bne.s	loc_6BBA
-		move.b	2(a1,d1.w),d0
-		sub.b	d0,obAniFrame(a0)
-		sub.b	d0,d1
-		move.b	1(a1,d1.w),d0
-		bra.s	loc_6B70
-; ===========================================================================
+; afChange = change to a different animation
+Anim_End_FD:
+		addq.b	#1,d0					; is the end flag = $FD?
+		bne.s	Anim_End_FC				; if not, branch
+		move.b	2(a1,d1.w),obAnim(a0)			; read next byte, run that animation
+; ---------------------------------------------------------------------------
 
-loc_6BBA:
-		addq.b	#1,d0
-		bne.s	loc_6BC4
-		move.b	2(a1,d1.w),obAnim(a0)
+; afRoutine = increment routine counter
+Anim_End_FC:
+		addq.b	#1,d0					; is the end flag = $FC?
+		bne.s	Anim_End_FB				; if not, branch
+		addq.b	#2,obRoutine(a0)			; advance primary routine counter
+; ---------------------------------------------------------------------------
 
-loc_6BC4:
-		addq.b	#1,d0
-		bne.s	loc_6BCC
-		addq.b	#2,obRoutine(a0)
+; afReset = reset animation and secondary object routine counter
+Anim_End_FB:
+		addq.b	#1,d0					; is the end flag = $FB?
+		bne.s	Anim_End				; if not, branch
+		move.b	#0,obAniFrame(a0)			; restart the animation from the beginning
+		clr.b	ob2ndRout(a0)				; reset secondary routine counter
+; ---------------------------------------------------------------------------
 
-loc_6BCC:
-		addq.b	#1,d0
-		bne.s	locret_6BDA
-		move.b	#0,obAniFrame(a0)
-		clr.b	ob2ndRout(a0)
-
-locret_6BDA:
-		rts
+Anim_End:
+		rts						; special flag handling done (or ID between $80-$F9 was used)
+; End of function AnimateSprite
